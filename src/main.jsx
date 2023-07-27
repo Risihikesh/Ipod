341608eb84f933eb4c7df80ef27ab994fb9c5b17
Rishikesh <krishikesh369@gmail.com>
2023-07-27 18:38:49 +0530

    updating few things

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './css/index.css';
+import ZingTouch from 'zingtouch';
+import App from './components/App.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
